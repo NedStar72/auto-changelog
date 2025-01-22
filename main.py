56e79e71be2e2e5208f61e93b5f9b75bd5193557
Dmitry Torgashin <torgashin@skbkontur.ru>
2025-01-22 15:42:14 +0500

    Add GitPython

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import os
+from git import Repo
+
 if __name__ == "__main__":
-  print("Hello, Python project!")
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+  repo = Repo(dir_path)
+  print(repo.head.commit.message.strip())
